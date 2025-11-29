b21d299f3fd5abea3fcf4e855b8c451d6539fe3b
prakashsingh62 <prakashkumarsing@gmail.com>
2025-11-29 15:39:09 +0530

    Delete autopilot directory

--- autopilot/action_types.py
+++
@@ -1,9 +0,0 @@
-class ActionType:
-    NO_ACTION = "no_action"
-    FOLLOW_UP_VENDOR = "follow_up_vendor"
-    FOLLOW_UP_CLIENT = "follow_up_client"
-    SEND_QUOTATION = "send_quotation"
-    SEND_CLARIFICATION = "send_clarification"
-    ESCALATE_VENDOR = "escalate_vendor"
-    CLOSE_RFQ = "close_rfq"
-    MARK_QUOTATION_RECEIVED = "mark_quotation_received"
